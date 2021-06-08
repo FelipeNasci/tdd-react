15f56c3bf21179c7919bb2bd48f293100f355cf5
felipenasci <dfnascimento@live.com>
2021-06-08 20:20:46 -0300

    test:should call props.addToCart() when button gets clicked

--- components/product-card.spec.js
+++ components/product-card.spec.js
@@ -1,25 +1,41 @@
 import ProductCard from './product-card';
-import { screen, render } from '@testing-library/react';
+import { screen, render, fireEvent } from '@testing-library/react';
 
 const product = {
   title: 'Clock',
   price: '80.00',
-  image: 'https://images-na.ssl-images-amazon.com/images/I/618MEYCaUQL._AC_SX522_.jpg'
-}
+  image: 'https://images-na.ssl-images-amazon.com/images/I/618MEYCaUQL._AC_SX522_.jpg',
+};
+
+const addToCart = jest.fn();
+
+const makeSut = () => {
+  render(<ProductCard addToCart={addToCart} product={product} />);
+};
 
 describe('ProductCard', () => {
   it('should render ProductCard component', () => {
-    render(<ProductCard  product={product}/>);
-
+    makeSut();
     expect(screen.getByTestId('product-card')).toBeInTheDocument();
   });
+
   it('should display proper content', () => {
-    render(<ProductCard  product={product}/>);
+    makeSut();
 
     expect(screen.getByText(product.title)).toBeInTheDocument();
     expect(screen.getByText(new RegExp(product.price, 'i'))).toBeInTheDocument();
     expect(screen.getByTestId('image')).toHaveStyle({
-      backgroundImage: product.image
+      backgroundImage: product.image,
     });
+  });
+
+  it('should call props.addToCart() when button gets clicked', async () => {
+    makeSut();
+
+    const button = screen.getByRole('button');
+    await fireEvent.click(button);
+
+    expect(addToCart).toHaveBeenCalledTimes(1);
+    expect(addToCart).toHaveBeenCalledWith(product);
   });
 });
