cbfbfd4504de50e28c4e0e7904ea17d8b8040fa8
felipenasci <dfnascimento@live.com>
2021-06-08 20:11:35 -0300

    feat: should display proper content

--- components/product-card.spec.js
+++ components/product-card.spec.js
@@ -1,10 +1,25 @@
 import ProductCard from './product-card';
 import { screen, render } from '@testing-library/react';
 
+const product = {
+  title: 'Clock',
+  price: '80.00',
+  image: 'https://images-na.ssl-images-amazon.com/images/I/618MEYCaUQL._AC_SX522_.jpg'
+}
+
 describe('ProductCard', () => {
   it('should render ProductCard component', () => {
-    render(<ProductCard />);
+    render(<ProductCard  product={product}/>);
 
     expect(screen.getByTestId('product-card')).toBeInTheDocument();
+  });
+  it('should display proper content', () => {
+    render(<ProductCard  product={product}/>);
+
+    expect(screen.getByText(product.title)).toBeInTheDocument();
+    expect(screen.getByText(new RegExp(product.price, 'i'))).toBeInTheDocument();
+    expect(screen.getByTestId('image')).toHaveStyle({
+      backgroundImage: product.image
+    });
   });
 });
