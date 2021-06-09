0d45a50ff7b7ba91b27aa7a4ecdd313494c4516f
felipenasci <dfnascimento@live.com>
2021-06-08 22:28:45 -0300

    should call props.doSearch() with the user input

--- components/search.spec.js
+++ components/search.spec.js
@@ -1,6 +1,6 @@
 import Search from './search.js';
 import { render, screen, fireEvent } from '@testing-library/react';
-
+import userEvent from '@testing-library/user-event';
 const doSearch = jest.fn();
 
 describe('Search', () => {
@@ -15,5 +15,17 @@ describe('Search', () => {
     const form = screen.getByRole('form');
     await fireEvent.submit(form);
     expect(doSearch).toHaveBeenCalledTimes(1);
+  });
+
+  it('should call props.doSearch() with the user input', async () => {
+    render(<Search doSearch={doSearch} />);
+    const form = screen.getByRole('form');
+    const input = screen.getByRole('textbox');
+    const inputText = 'any_text';
+
+    await userEvent.type(input, inputText);
+    await fireEvent.submit(form);
+
+    expect(doSearch).toHaveBeenCalledWith(inputText);
   });
 });
